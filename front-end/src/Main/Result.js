7e00a49bf9aff2f2511fa9f1737ace1e6d4c2a19
jaeyong <57833917+kwonjaeyong1@users.noreply.github.com>
2022-06-10 09:05:09 +0900

    View Result

--- front-end/src/Main/Result.js
+++ front-end/src/Main/Result.js
@@ -1,8 +1,8 @@
 import './Result.css';
-
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/effect-cards";
+import axios from "axios";
 
 // import required modules
 import SwiperCore, { EffectCards, Keyboard } from "swiper";
@@ -13,7 +13,7 @@ const ResultDetail = ({title}) => {
     return (
         <div class = "resultDetail">
             <div class = "image">
-                <img src = "" alt = "Waiting for Result"></img>
+                <img src = 'public/',{mouse_name}_{title},'png' = "Waiting for Result"></img>
             </div>
             <div class = "text_r">
                 <div> {title} <sup>improved</sup></div>
@@ -30,7 +30,7 @@ const Result = () => {
             <div class = "Rtop">
                 <div class = "mainTitle">Result</div>
                 <div></div>
-                <a href = "./result.csv" download><button class = "csv_icon"></button></a>
+                <a href = {axios.get("http://127.0.0.1:8000/member/download")} download><button class = "csv_icon"></button></a>
             </div>
             <div class = "Rbottom">
                 <Swiper
